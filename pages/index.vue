a8225d8f42cc25c3856729a37aaa894e68e6f10c
g0ncharuk <a.v.honcharuk@gmail.com>
2018-11-13 02:34:25 +0200

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="section is-large">
     <div class="container has-text-centered">
-      <h1 class="title">Hi, I'm Chimezie Enyinnaya</h1>
-      <h2 class="subtitle">a Software Developer based in Lagos, Nigeria.</h2>
+      <h1 class="title">Hi, I'm Dev</h1>
+      <h2 class="subtitle">a Software Developer.</h2>
     </div>
   </section>
 </template>
