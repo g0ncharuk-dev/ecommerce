8f03e3ae4d62069b94942774a253ee441cc018fc
g0ncharuk <a.v.honcharuk@gmail.com>
2018-11-13 02:22:36 +0200

    first commit

---
+++ pages/contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contact Me</h1>
+      <p>
+        You can follow me on Twitter: <a href="https://twitter.com/ammezie" target="_blank">@ammezie</a>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "contact"
+    }
+</script>
+
+<style scoped>
+
+</style>
